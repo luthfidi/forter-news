f4b5fd9791038bf9cb7e1e4cc9e1246fc6ae9161
hmiftachul11 <hmiftachul11@gmail.com>
2025-10-17 17:24:38 +0700

    feat: adjust using base sepolia

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { base } from 'viem/chains';
+import { baseSepolia } from 'viem/chains';
 
 import { config } from '@/lib/wagmi';
 import '@rainbow-me/rainbowkit/styles.css';
@@ -47,7 +47,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider
           apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-          chain={base}
+          chain={baseSepolia}
           config={{
             appearance: {
               mode: 'auto',
